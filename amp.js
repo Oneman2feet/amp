71a94203bed76874affcd11d9af30b926ece2707
Zachary Zimmerman <Oneman2feet@gmail.com>
2017-11-23 15:42:30 -0800

    ios fix try

--- amp.js
+++ amp.js
@@ -1,3 +1,20 @@
+// for iOS
+window.addEventListener('touchstart', function() {
+
+  // create empty buffer
+  var buffer = myContext.createBuffer(1, 1, 22050);
+  var source = myContext.createBufferSource();
+  source.buffer = buffer;
+
+  // connect to output (your speakers)
+  source.connect(myContext.destination);
+
+  // play the file
+  source.noteOn(0);
+  alert('attempting to unlock audio...');
+
+}, false);
+
 function gotStream(stream) {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     var audioContext = new AudioContext();
