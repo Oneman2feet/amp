75fd5b632f83f78beba91d7236a02b5b39761746
Zachary Zimmerman <Oneman2feet@gmail.com>
2017-11-23 17:57:14 -0800

    latency test

--- amp.js
+++ amp.js
@@ -1,6 +1,7 @@
 var audioContext;
 
 // for iOS
+/*
 var isUnlocked = false;
 function unlock() {
   if (isUnlocked) return;
@@ -19,7 +20,24 @@ function unlock() {
       document.body.innerHTML = "Sound is unlocked!";
     }
   }, 0);
+}
+*/
 
+// context state at this time is `undefined` in iOS8 Safari
+function unlock() {
+  if (audioContext.state === 'suspended') {
+    var resume = function () {
+      audioContext.resume();
+
+      setTimeout(function () {
+        if (ctx.state === 'running') {
+          document.body.removeEventListener('touchend', resume, false);
+        }
+      }, 0);
+    };
+
+    document.body.addEventListener('touchend', resume, false);
+  }
 }
 
 function gotStream(stream) {
@@ -31,6 +49,8 @@ function gotStream(stream) {
 
     // Connect it to the destination to hear yourself (or any other node for processing!)
     mediaStreamSource.connect(audioContext.destination);
+    unlock();
+    alert(audioContext.baseLatency);
 }
 
 function onError() {
@@ -40,4 +60,83 @@ function onError() {
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 navigator.getUserMedia({audio:true}, gotStream, onError);
 
-window.addEventListener('touchstart', unlock, false);
+//window.addEventListener('touchstart', unlock, false);
+
+
+// ios unlock
+/*
+var ctx = null, usingWebAudio = true;
+
+try {
+  if (typeof AudioContext !== 'undefined') {
+      ctx = new AudioContext();
+  } else if (typeof webkitAudioContext !== 'undefined') {
+      ctx = new webkitAudioContext();
+  } else {
+      usingWebAudio = false;
+  }
+} catch(e) {
+    usingWebAudio = false;
+}
+
+// context state at this time is `undefined` in iOS8 Safari
+if (usingWebAudio && ctx.state === 'suspended') {
+  var resume = function () {
+    ctx.resume();
+
+    setTimeout(function () {
+      if (ctx.state === 'running') {
+        document.body.removeEventListener('touchend', resume, false);
+      }
+    }, 0);
+  };
+
+  document.body.addEventListener('touchend', resume, false);
+}
+*/
+
+// latency
+/*
+
+console.log("test started");
+console.log("MediaTrackSettings.latency: " + navigator.mediaDevices.getSupportedConstraints().latency);
+navigator.getUserMedia = (navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia);
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+console.log('audioCtx: ' + audioCtx);
+
+if (navigator.getUserMedia) {
+  console.log('getUserMedia supported.');
+  if (navigator.mediaDevices.getUserMedia) {
+    console.log("navigator.mediaDevices.getUserMedia exists");
+    var p = navigator.mediaDevices.getUserMedia({ audio: { latency: 0.05, echoCancellation: false, mozNoiseSuppression: true, mozAutoGainControl: false} });
+    //var p = navigator.mediaDevices.getUserMedia({ audio: true });
+
+    p.then(function(stream) {
+      console.log("navigator.mediaDevices.getUserMedia started processStream(stream)");
+      processStream(stream);
+      //console.dir(stream);
+      //console.dir(stream.getAudioTracks());
+      //console.dir(stream.getAudioTracks()[0].getSettings());
+      //console.dir(stream.getAudioTracks()[0].getCapabilities());
+      //console.dir(stream.getAudioTracks()[0].getConstraints());
+    });
+    p.catch(function(err) { console.log(err.name); });
+  }
+} else {
+  console.log('getUserMedia not supported on your browser!');
+}
+
+function processStream(stream) {
+  console.log("ProcessingStream started");
+  //get source
+  source = audioCtx.createMediaStreamSource(stream);
+  console.log("ProcessingStream source is created. source: " + source);
+  source.connect(audioCtx.destination);
+}
+
+*/
