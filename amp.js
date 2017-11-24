da19d197e5a7bb8e21684dade1c3901b19189be9
Zachary Zimmerman <Oneman2feet@gmail.com>
2017-11-23 18:18:58 -0800

    help

--- amp.js
+++ amp.js
@@ -5,7 +5,7 @@ function gotStream(stream) {
     audioContext = new AudioContext();
     var mediaStreamSource = audioContext.createMediaStreamSource(stream);
     mediaStreamSource.connect(audioContext.destination);
-    unlock();
+    alert(navigator.mediaDevices.getSupportedConstraints().latency);
 }
 
 function onError() {
@@ -14,54 +14,8 @@ function onError() {
 
 function start() {
   navigator.getUserMedia({audio:true}, gotStream, onError);
+  //navigator.mediaDevices.getUserMedia({ audio: { latency: 0.05, echoCancellation: false, mozNoiseSuppression: true, mozAutoGainControl: false} });
 }
-
-// latency
-/*
-
-console.log("test started");
-console.log("MediaTrackSettings.latency: " + navigator.mediaDevices.getSupportedConstraints().latency);
-navigator.getUserMedia = (navigator.getUserMedia ||
-                            navigator.webkitGetUserMedia ||
-                            navigator.mozGetUserMedia ||
-                            navigator.msGetUserMedia);
-
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-console.log('audioCtx: ' + audioCtx);
-
-if (navigator.getUserMedia) {
-  console.log('getUserMedia supported.');
-  if (navigator.mediaDevices.getUserMedia) {
-    console.log("navigator.mediaDevices.getUserMedia exists");
-    var p = navigator.mediaDevices.getUserMedia({ audio: { latency: 0.05, echoCancellation: false, mozNoiseSuppression: true, mozAutoGainControl: false} });
-    //var p = navigator.mediaDevices.getUserMedia({ audio: true });
-
-    p.then(function(stream) {
-      console.log("navigator.mediaDevices.getUserMedia started processStream(stream)");
-      processStream(stream);
-      //console.dir(stream);
-      //console.dir(stream.getAudioTracks());
-      //console.dir(stream.getAudioTracks()[0].getSettings());
-      //console.dir(stream.getAudioTracks()[0].getCapabilities());
-      //console.dir(stream.getAudioTracks()[0].getConstraints());
-    });
-    p.catch(function(err) { console.log(err.name); });
-  }
-} else {
-  console.log('getUserMedia not supported on your browser!');
-}
-
-function processStream(stream) {
-  console.log("ProcessingStream started");
-  //get source
-  source = audioCtx.createMediaStreamSource(stream);
-  console.log("ProcessingStream source is created. source: " + source);
-  source.connect(audioCtx.destination);
-}
-
-*/
-
 
 // for iOS audio unlocking
 var isUnlocked = false;
@@ -83,3 +37,5 @@ function unlock() {
     }
   }, 0);
 }
+
+window.addEventListener('touchstart', unlock, false);
