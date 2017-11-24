29c37ed775f45a73615ac8f1cd1417c80abd40d6
Zachary Zimmerman <Oneman2feet@gmail.com>
2017-11-23 17:23:48 -0800

    was wrong I think it needs the tap actually...

--- amp.js
+++ amp.js
@@ -1,5 +1,27 @@
 var audioContext;
 
+// for iOS
+var isUnlocked = false;
+function unlock() {
+  if (isUnlocked) return;
+
+  // create empty buffer and play it
+  var buffer = audioContext.createBuffer(1, 1, 22050);
+  var source = audioContext.createBufferSource();
+  source.buffer = buffer;
+  source.connect(audioContext.destination);
+  source.start(0);
+
+  // by checking the play state after some time, we know if we're really unlocked
+  setTimeout(function() {
+    if((source.playbackState === source.PLAYING_STATE || source.playbackState === source.FINISHED_STATE)) {
+      isUnlocked = true;
+      document.body.innerHTML = "Sound is unlocked!";
+    }
+  }, 0);
+
+}
+
 function gotStream(stream) {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     audioContext = new AudioContext();
@@ -17,3 +39,5 @@ function onError() {
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
 navigator.getUserMedia({audio:true}, gotStream, onError);
+
+window.addEventListener('touchstart', unlock, false);
